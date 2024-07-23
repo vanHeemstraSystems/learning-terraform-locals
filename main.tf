6584ca401873c56e90720b42f655afd2fbb2bcb9
Willem van Heemstra <wvanheemstra@icloud.com>
2024-07-23 12:33:40 +0000

    Update

--- main.tf
+++ main.tf
@@ -12,3 +12,11 @@ resource azurerm_resource_group main {
   name = "rg-${local.name}-${random_string.main.result}"
   location = var.location
 }
+
+output rando {
+  value = random_string.main.result  
+}
+
+output rg_id {
+  value = azurerm_resource_group.main.id
+}
